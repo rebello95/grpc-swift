633c624dd02b3087e911866786fb04a0ca66194d
Michael Rebello <me@michaelrebello.com>
2019-09-04 14:11:22 -0700

    kill sike

--- scripts/vendor-boringssl.sh
+++ scripts/vendor-boringssl.sh
@@ -27,6 +27,7 @@ rm -rf $DSTROOT/include
 rm -rf $DSTROOT/ssl
 rm -rf $DSTROOT/crypto
 rm -rf $DSTROOT/err_data.c
+rm -rf $DSTROOT/third_party
 
 PATTERNS=(
 'include/openssl/*.h'
@@ -40,8 +41,6 @@ PATTERNS=(
 'crypto/*/*/*.c'
 'third_party/fiat/*.h'
 'third_party/fiat/*.c'
-'third_party/sike/*.h'
-'third_party/sike/*.c'
 )
 
 EXCLUDES=(
